204c694955efbe6d96dcb024cd8de9ebade532e8
Jack Pritchard <jack@noface.co.uk>
2021-08-01 15:40:50 +0100

    Include loading bar in appp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,44 +1,65 @@
-import { useCallback } from 'react';
-import type { AppProps } from 'next/app';
-import { setContext } from '@apollo/client/link/context';
+import "nprogress/nprogress.css";
+
+import * as React from "react";
+import { useCallback } from "react";
+import type { AppProps } from "next/app";
+import dynamic from "next/dynamic";
+import { ThemeProvider } from "styled-components";
+import { setContext } from "@apollo/client/link/context";
 import {
-  ApolloClient,
-  ApolloProvider,
-  createHttpLink,
-  InMemoryCache,
-} from '@apollo/client';
+	ApolloClient,
+	ApolloProvider,
+	createHttpLink,
+	InMemoryCache,
+} from "@apollo/client";
 
 let apolloClient: ApolloClient<any>;
 
+import { GlobalStyle } from "sonovate";
+import { ThemeDefault } from "sonovate";
+
+const TopProgressBar = dynamic(
+	() => {
+		return import("../organisms/progress");
+	},
+	{ ssr: false }
+);
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const createApolloClient = useCallback(() => {
-    const httpLink = createHttpLink({
-      uri: 'https://graphql.contentful.com/content/v1/spaces/jgxvzzx7ps77',
-    });
+	const createApolloClient = useCallback(() => {
+		const httpLink = createHttpLink({
+			uri: "https://graphql.contentful.com/content/v1/spaces/jgxvzzx7ps77",
+		});
 
-    const authLink = setContext((_, { headers }) => ({
-      headers: {
-        ...headers,
-        authorization: 'Bearer AEf7QMYxPL9rGzq0iYw8vNWzbRvGEhLrtPXHYWYYE_I',
-      }
-    }));
+		const authLink = setContext((_, { headers }) => ({
+			headers: {
+				...headers,
+				authorization: "Bearer AEf7QMYxPL9rGzq0iYw8vNWzbRvGEhLrtPXHYWYYE_I",
+			},
+		}));
 
-    const apolloClient = new ApolloClient({
-      cache: new InMemoryCache(),
-      link: authLink.concat(httpLink),
-      ssrMode: typeof window === 'undefined',
-    });
+		const apolloClient = new ApolloClient({
+			cache: new InMemoryCache(),
+			link: authLink.concat(httpLink),
+			ssrMode: typeof window === "undefined",
+		});
 
-    return apolloClient;
-  }, []);
+		return apolloClient;
+	}, []);
 
-  let client = apolloClient ?? createApolloClient();
+	let client = apolloClient ?? createApolloClient();
 
-  return (
-    <ApolloProvider client={client}>
-      <Component {...pageProps} />
-    </ApolloProvider>
-  );
+	return (
+		<React.Fragment>
+			<TopProgressBar />
+			<ApolloProvider client={client}>
+				<ThemeProvider theme={ThemeDefault}>
+					<GlobalStyle />
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</ApolloProvider>
+		</React.Fragment>
+	);
 }
 
-export default MyApp
+export default MyApp;
