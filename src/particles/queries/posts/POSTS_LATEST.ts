01587b962e2f50241232a0019097b153404a71bb
Jack Pritchard <jack@noface.co.uk>
2021-08-01 17:23:44 +0100

    Order posts by title

--- src/particles/queries/posts/POSTS_LATEST.ts
+++ src/particles/queries/posts/POSTS_LATEST.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 const POSTS_LATEST = gql`
 	query POSTS_LATEST {
-		blogPostCollection(limit: 10) {
+		blogPostCollection(limit: 10, order: title_DESC) {
 			items {
 				body
 				preface
