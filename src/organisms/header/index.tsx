5e6b0589b2101e5a840b3b620672b7e04e8e27ed
Jack Pritchard <jack@noface.co.uk>
2021-08-01 15:40:35 +0100

    Add header logos and links

--- src/organisms/header/index.tsx
+++ src/organisms/header/index.tsx
@@ -1,17 +1,23 @@
 import React from "react";
 
-import Link from "next/link";
-
 import HeaderComponent from "./header.styles";
+
+import { Link } from "sonovate";
+import { Logo } from "sonovate";
 
 const Header = () => {
 	return (
 		<HeaderComponent className="header">
 			<div className="header__contents">
-				<Link href="/">
-					<span className="hidden">Sonovate Homepage</span>
-				</Link>
-				<nav className="header__menu"></nav>
+				<div className="header__logo">
+					<Link to="/">
+						<Logo />
+						<span className="hidden">Sonovate Homepage</span>
+					</Link>
+				</div>
+				<nav className="header__menu">
+					<Link to="/">Homepage</Link>
+				</nav>
 			</div>
 		</HeaderComponent>
 	);
