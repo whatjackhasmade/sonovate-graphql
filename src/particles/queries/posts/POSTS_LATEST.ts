d41bd204fd568fd92e0e68fdc8063b57391654e1
Jack Pritchard <jack@noface.co.uk>
2021-08-01 17:24:32 +0100

    Change query direction of title order

--- src/particles/queries/posts/POSTS_LATEST.ts
+++ src/particles/queries/posts/POSTS_LATEST.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 const POSTS_LATEST = gql`
 	query POSTS_LATEST {
-		blogPostCollection(limit: 10, order: title_DESC) {
+		blogPostCollection(limit: 10, order: title_ASC) {
 			items {
 				body
 				preface
